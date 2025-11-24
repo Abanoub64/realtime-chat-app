34bd92a40458e0b78d1a9970fcd79591246dc547
Abanoub Milad <abanoubmilad64@gmail.com>
2025-11-24 04:29:30 +0200

    Ready for deployment

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Button from "@/components/ui/Button";
-import { db } from "@/lib/db";
-
-
-
-export default async function Home() {
-  return <Button variant={"default"}>HEllcdsfaesdefdO</Button>
-}
