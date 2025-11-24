dc0c81fe1cdcdb75035b49a28e228bb9af29d273
Abanoub Milad <abanoubmilad64@gmail.com>
2025-11-24 05:26:11 +0200

    Fixed some bugs in the backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://realtime-chat-app-vat5.onrender.com/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
